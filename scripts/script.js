c3609f56e7b7968cfa91859099df2da585a60684
BillyValt <123888056+BillyValt@users.noreply.github.com>
2023-12-20 09:01:25 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -31,7 +31,7 @@ function pickWinner() {
     </div>
   `;
 
-  winnerContainerEl.style.backgroundColor = 'white';
+  winnerContainerEl.style.backgroundColor = 'rgb(227, 227, 227)';
 
   intervalId1 = setInterval(() => {
     const randomPerson2 = Math.floor(Math.random() * personTotalNumber);;
@@ -145,7 +145,7 @@ function resetPrize() {
       <img class="container__img js-prize-animation-disappearing" id="img-id-0" src="images/mystery__box1.png">
     </div>
   `;
-  winnerContainerEl.style.backgroundColor = 'white';
+  winnerContainerEl.style.backgroundColor = 'rgb(227, 227, 227)';
 
   const copyResultImage = document.querySelector('.container__img');
   copyResultImage.classList.add('prize-animation');

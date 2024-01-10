ebfc96a0d911105fad9113594408a0a2d80cf92d
BillyValt <123888056+BillyValt@users.noreply.github.com>
2024-01-11 01:20:15 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
 import { participants, chosenWinner, threeWinners } from './dataParticipants.js';
+import createStars from './stars.js';
+import runSnowflakes from './snowflakes.js';
+
+runSnowflakes();
+// createStars();
+
 
 const startButton = document.querySelector('.js-start__btn');
 const resetButton = document.querySelector('.js-reset__btn');
@@ -11,7 +17,6 @@ const dotsWrapper = document.querySelector('.js-dots-wrapper')
 
 let savedDemoIsOn = JSON.parse(localStorage.getItem('setting'));
 let savedThreeWinners = JSON.parse(localStorage.getItem('setting1'));
-console.log(localStorage)
 
 let winner;
 let wishWinner = chosenWinner[0].personName;
@@ -23,8 +28,7 @@ function pickWinner() {
   const randomPerson = Math.floor(Math.random() * personTotalNumber);
   let intervalId1;
 
-  resultEl.innerHTML =
-    `
+  resultEl.innerHTML = `
     <div class="winner__incription">Подарок:</div>
     <div class="container__element">
       <img class="container__img" id="img-id-0" src="images/mystery__box1.png">
@@ -136,7 +140,7 @@ function pickPrize() {
 resultImage.classList.add('prize-animation');
 
 function resetPrize() {
-  winnerNameEl.innerHTML = '&#10053;&#10053;&#10053;&#10053;&#10053; &#10053;&#10053;&#10053;&#10053;&#10053;';
+  winnerNameEl.innerHTML = '<span class="name-stars1">&#10032;&#10032;&#10032;&#10032;&#10032; &#10032;&#10032;&#10032;&#10032;&#10032;</span>';
 
   resultEl.innerHTML =
     `
@@ -167,47 +171,6 @@ resetButton.addEventListener('click', () => {
 
   resetPrize();
 });
-
-
-//===STARS BACKGROUND===
-function createSnowflake() {
-  const snowflake = document.createElement('div');
-  snowflake.className = 'snowflake';
-  document.body.appendChild(snowflake);
-
-  const size = Math.random() * 4 + 3; // Random size between 5 and 15
-  snowflake.style.width = `${size}px`;
-  snowflake.style.height = `${size}px`;
-
-  const left = Math.random() * window.innerWidth;
-  const top = Math.random() * window.innerHeight;
-  snowflake.style.left = `${left}px`;
-  snowflake.style.top = `-10vh`;
-
-  const delay = Math.random() * 11; // Random delay between 0 and 5 seconds
-  snowflake.style.animationDelay = `-${delay}s`;
-
-  snowflake.addEventListener('animationiteration', () => {
-    document.body.removeChild(snowflake);
-    createSnowflake();
-  });
-}
-
-function createSnowflakes() {
-  const numberOfSnowflakes = 110; // Adjust the number of initial snowflakes
-  for (let i = 0; i < numberOfSnowflakes; i++) {
-    createSnowflake();
-  }
-}
-
-createSnowflakes();
-
-window.addEventListener('resize', createSnowflakes);
-
-// setInterval(() => { 
-//   createSnowflakes();
-// }, 5000)
-
 
 
 
